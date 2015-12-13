77858a8a8620689043df1d1f74c2e820ad43567b
Erik Humphrey <ContactErikH@gmail.com>
2015-12-13 15:37:58 -0500

    Initial commit
    Initial commit

---
+++ HumphreyErik20CardWars/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace HumphreyErik20CardWars
+{
+    partial class frmCardWars
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewGameNextCard = new System.Windows.Forms.Button();
+            this.btnQuitStop = new System.Windows.Forms.Button();
+            this.lblScoreCPUTitle = new System.Windows.Forms.Label();
+            this.lblScoreUserTitle = new System.Windows.Forms.Label();
+            this.lblScoreCPU = new System.Windows.Forms.Label();
+            this.lblScoreUser = new System.Windows.Forms.Label();
+            this.lblStatusMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnNewGameNextCard
+            // 
+            this.btnNewGameNextCard.Location = new System.Drawing.Point(434, 440);
+            this.btnNewGameNextCard.Name = "btnNewGameNextCard";
+            this.btnNewGameNextCard.Size = new System.Drawing.Size(75, 23);
+            this.btnNewGameNextCard.TabIndex = 0;
+            this.btnNewGameNextCard.Text = "New game";
+            this.btnNewGameNextCard.UseVisualStyleBackColor = true;
+            this.btnNewGameNextCard.Click += new System.EventHandler(this.btnNewGameNextCard_Click);
+            // 
+            // btnQuitStop
+            // 
+            this.btnQuitStop.Location = new System.Drawing.Point(434, 469);
+            this.btnQuitStop.Name = "btnQuitStop";
+            this.btnQuitStop.Size = new System.Drawing.Size(75, 23);
+            this.btnQuitStop.TabIndex = 1;
+            this.btnQuitStop.Text = "Quit game";
+            this.btnQuitStop.UseVisualStyleBackColor = true;
+            this.btnQuitStop.Click += new System.EventHandler(this.btnQuitStop_Click);
+            // 
+            // lblScoreCPUTitle
+            // 
+            this.lblScoreCPUTitle.AutoSize = true;
+            this.lblScoreCPUTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblScoreCPUTitle.Font = new System.Drawing.Font("Bebas Neue Bold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreCPUTitle.Location = new System.Drawing.Point(163, 45);
+            this.lblScoreCPUTitle.Name = "lblScoreCPUTitle";
+            this.lblScoreCPUTitle.Size = new System.Drawing.Size(41, 29);
+            this.lblScoreCPUTitle.TabIndex = 3;
+            this.lblScoreCPUTitle.Text = "CPU";
+            // 
+            // lblScoreUserTitle
+            // 
+            this.lblScoreUserTitle.AutoSize = true;
+            this.lblScoreUserTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblScoreUserTitle.Font = new System.Drawing.Font("Bebas Neue Bold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreUserTitle.Location = new System.Drawing.Point(46, 45);
+            this.lblScoreUserTitle.Name = "lblScoreUserTitle";
+            this.lblScoreUserTitle.Size = new System.Drawing.Size(43, 29);
+            this.lblScoreUserTitle.TabIndex = 4;
+            this.lblScoreUserTitle.Text = "YOU";
+            // 
+            // lblScoreCPU
+            // 
+            this.lblScoreCPU.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblScoreCPU.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreCPU.Location = new System.Drawing.Point(156, 74);
+            this.lblScoreCPU.Name = "lblScoreCPU";
+            this.lblScoreCPU.Size = new System.Drawing.Size(54, 54);
+            this.lblScoreCPU.TabIndex = 6;
+            this.lblScoreCPU.Text = "0";
+            this.lblScoreCPU.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblScoreUser
+            // 
+            this.lblScoreUser.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblScoreUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreUser.Location = new System.Drawing.Point(41, 74);
+            this.lblScoreUser.Name = "lblScoreUser";
+            this.lblScoreUser.Size = new System.Drawing.Size(54, 54);
+            this.lblScoreUser.TabIndex = 8;
+            this.lblScoreUser.Text = "0";
+            this.lblScoreUser.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblStatusMessage
+            // 
+            this.lblStatusMessage.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblStatusMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatusMessage.Location = new System.Drawing.Point(21, 155);
+            this.lblStatusMessage.Name = "lblStatusMessage";
+            this.lblStatusMessage.Size = new System.Drawing.Size(226, 30);
+            this.lblStatusMessage.TabIndex = 9;
+            this.lblStatusMessage.Text = "Click \"New game\" to begin.";
+            this.lblStatusMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmCardWars
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 540);
+            this.Controls.Add(this.lblStatusMessage);
+            this.Controls.Add(this.lblScoreUser);
+            this.Controls.Add(this.lblScoreCPU);
+            this.Controls.Add(this.lblScoreUserTitle);
+            this.Controls.Add(this.lblScoreCPUTitle);
+            this.Controls.Add(this.btnQuitStop);
+            this.Controls.Add(this.btnNewGameNextCard);
+            this.Name = "frmCardWars";
+            this.Text = "Card Wars";
+            this.Load += new System.EventHandler(this.frmCardWars_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewGameNextCard;
+        private System.Windows.Forms.Button btnQuitStop;
+        private System.Windows.Forms.Label lblScoreCPUTitle;
+        private System.Windows.Forms.Label lblScoreUserTitle;
+        private System.Windows.Forms.Label lblScoreCPU;
+        private System.Windows.Forms.Label lblScoreUser;
+        private System.Windows.Forms.Label lblStatusMessage;
+    }
+}
+
---
+++ HumphreyErik20CardWars/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HumphreyErik20CardWars
+{
+    public partial class frmCardWars : Form
+    {
+        // Global declarations
+        Random rnd = new Random();
+        int scoreCPU, scoreUser, cardIndex;
+        int[] cardNumber = new int[52];
+        Image[] picCards = new Image[52];
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont, IntPtr pdv, [System.Runtime.InteropServices.In] ref uint pcFonts);
+        private PrivateFontCollection fonts = new PrivateFontCollection();
+        Font BNB;
+
+        public frmCardWars()
+        {
+            InitializeComponent();
+
+            byte[] fontData = Properties.Resources.BNB;
+            IntPtr fontPtr = System.Runtime.InteropServices.Marshal.AllocCoTaskMem(fontData.Length);
+            System.Runtime.InteropServices.Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+            uint dummy = 0;
+            fonts.AddMemoryFont(fontPtr, Properties.Resources.BNB.Length);
+            AddFontMemResourceEx(fontPtr, (uint)Properties.Resources.BNB.Length, IntPtr.Zero, ref dummy);
+            System.Runtime.InteropServices.Marshal.FreeCoTaskMem(fontPtr);
+
+            BNB = new Font(fonts.Families[0], 24.0F); // Declare bundled Bebas Neue Bold font
+        }
+
+        private void frmCardWars_Load(object sender, EventArgs e)
+        {
+            picCards[0] = Properties.Resources.H2;
+            picCards[1] = Properties.Resources.H3;
+            picCards[2] = Properties.Resources.H4;
+            picCards[3] = Properties.Resources.H5;
+            picCards[4] = Properties.Resources.H6;
+            picCards[5] = Properties.Resources.H7;
+            picCards[6] = Properties.Resources.H8;
+            picCards[7] = Properties.Resources.H9;
+            picCards[8] = Properties.Resources.H10;
+            picCards[9] = Properties.Resources.HJ;
+            picCards[10] = Properties.Resources.HQ;
+            picCards[11] = Properties.Resources.HK;
+            picCards[12] = Properties.Resources.HA;
+            picCards[13] = Properties.Resources.D2;
+            picCards[14] = Properties.Resources.D3;
+            picCards[15] = Properties.Resources.D4;
+            picCards[16] = Properties.Resources.D5;
+            picCards[17] = Properties.Resources.D6;
+            picCards[18] = Properties.Resources.D7;
+            picCards[19] = Properties.Resources.D8;
+            picCards[20] = Properties.Resources.D9;
+            picCards[21] = Properties.Resources.D10;
+            picCards[22] = Properties.Resources.DJ;
+            picCards[23] = Properties.Resources.DQ;
+            picCards[24] = Properties.Resources.DK;
+            picCards[25] = Properties.Resources.DA;
+            picCards[26] = Properties.Resources.C2;
+            picCards[27] = Properties.Resources.C3;
+            picCards[28] = Properties.Resources.C4;
+            picCards[29] = Properties.Resources.C5;
+            picCards[30] = Properties.Resources.C6;
+            picCards[31] = Properties.Resources.C7;
+            picCards[32] = Properties.Resources.C8;
+            picCards[33] = Properties.Resources.C9;
+            picCards[34] = Properties.Resources.C10;
+            picCards[35] = Properties.Resources.CJ;
+            picCards[36] = Properties.Resources.CQ;
+            picCards[37] = Properties.Resources.CK;
+            picCards[38] = Properties.Resources.CA;
+            picCards[39] = Properties.Resources.S2;
+            picCards[40] = Properties.Resources.S3;
+            picCards[41] = Properties.Resources.S4;
+            picCards[42] = Properties.Resources.S5;
+            picCards[43] = Properties.Resources.S6;
+            picCards[44] = Properties.Resources.S7;
+            picCards[45] = Properties.Resources.S8;
+            picCards[46] = Properties.Resources.S9;
+            picCards[47] = Properties.Resources.S10;
+            picCards[48] = Properties.Resources.SJ;
+            picCards[49] = Properties.Resources.SQ;
+            picCards[50] = Properties.Resources.SK;
+            picCards[51] = Properties.Resources.SA;
+        }
+
+        private void btnNewGameNextCard_Click(object sender, EventArgs e)
+        {
+            if (btnNewGameNextCard.Text == "New game")
+            {
+                btnNewGameNextCard.Text = "Next card";
+                btnQuitStop.Text = "Stop game";
+                scoreCPU = scoreUser = 0;
+                lblScoreCPUTitle.Font = BNB;
+
+
+            }
+            else
+            {
+                btnNewGameNextCard.Text = "New game";
+            }
+
+
+        }
+
+        private void btnQuitStop_Click(object sender, EventArgs e)
+        {
+            if (btnQuitStop.Text == "Quit game")
+                this.Close();
+            else
+            {
+                if (scoreUser > scoreCPU)
+                {
+
+                }
+                btnQuitStop.Text = "Quit game";
+            }
+        }
+    }
+}
